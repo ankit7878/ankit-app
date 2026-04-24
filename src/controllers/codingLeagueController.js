28929249d406f7c7755e09889158a4f5bc34f701
Ankit Morker <ankitmorker@Ankits-Laptop.local>
2026-04-24 16:53:55 +0530

    findById changes

--- src/controllers/codingLeagueController.js
+++ src/controllers/codingLeagueController.js
@@ -2,7 +2,7 @@ const CodingLeagueVisitors = require('../models/CodingLeagueVisitors');
 
 exports.getVisitors = async (req, res) => {
   try {
-    const visitors = await CodingLeagueVisitors.find().sort({ createdAt: -1 });
+    const visitors = await CodingLeagueVisitors.findById("69eb51bcb246de335234c931");
     res.json(visitors);
   } catch (err) {
     res.status(500).json({ message: err.message });
