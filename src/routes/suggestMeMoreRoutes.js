46d22da8ac23c6be9d5e7dae3aa22f0b3babfb18
Ankit Morker <ankitmorker@Ankits-Laptop.local>
2026-04-26 20:10:43 +0530

    small filename change

--- src/routes/suggestMeMoreRoutes.js
+++ src/routes/suggestMeMoreRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/suggestMeMoreController');
+const controller = require('../controllers/suggestMeMoreController.js');
 
 router.get('/getDetails', controller.getDetails);
 
