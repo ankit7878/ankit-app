2767dbd56aa1e3a2fb347de75eb2116a8a8217fc
Ankit Morker <ankitmorker@Ankits-Laptop.local>
2026-04-26 20:28:24 +0530

    small changes

--- src/controllers/ankitProController.js
+++ src/controllers/ankitProController.js
@@ -2,7 +2,7 @@ const AnkitProDetails = require('../models/AnkitProDetails');
 
 exports.getDetails = async (req, res) => {
   try {
-    const visitors = await AnkitProDetails.findById("69eb51bcb246de335234c931");
+    const visitors = await AnkitProDetails.findById("69ee26c0b439728ac6a592bb");
     res.json(visitors);
   } catch (err) {
     res.status(500).json({ message: err.message });
